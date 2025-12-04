361b372f359b67c0696ed48271294b990dfcdcdc
cxndizz <first.sr2499@gmail.com>
2025-12-04 05:13:05 +0000

    Update VPS Prod

--- frontend-admin/src/utils/axios.js
+++ frontend-admin/src/utils/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000', // URL ของ Backend NestJS
+  baseURL: 'https://api.fortestonlyme.online',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
--- frontend-liff/src/utils/axios.js
+++ frontend-liff/src/utils/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000', // ชี้ไปที่ Backend ของเรา
+  baseURL: 'https://api.fortestonlyme.online',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
